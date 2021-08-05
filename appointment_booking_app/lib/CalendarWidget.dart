38237fb15eda037dccc6ca2accc5cffb060a287a
rahul6975 <rahulya7569@gmail.com>
2021-08-05 13:16:47 +0530

    calendar widget added

--- appointment_booking_app/lib/CalendarWidget.dart
+++ appointment_booking_app/lib/CalendarWidget.dart
@@ -1,15 +1,28 @@
+import 'package:appointment_booking_app/EventProvider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+import 'AppointmentsDataSource.dart';
 
 class CalendarWidget extends StatelessWidget {
   const CalendarWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final events = Provider
+        .of<EventProvider>(context)
+        .events;
     return SfCalendar(
+      dataSource: AppointmentsDataSource(events),
       view: CalendarView.month,
       initialSelectedDate: DateTime.now(),
       cellBorderColor: Colors.transparent,
+      onLongPress: (details) {
+        final provider = Provider.of<EventProvider>(context, listen: false);
+        provider.setDate(details.date!);
+        showModalBottomSheet(context: context, builder: (context) => TasksWidget(),);
+      },
     );
   }
 }
