3313d63a85635d2dcb803c5218a3b735715c9509
rahul6975 <rahulya7569@gmail.com>
2021-08-05 09:34:44 +0530

    model class created

--- appointment_booking_app/lib/Event.dart
+++ appointment_booking_app/lib/Event.dart
@@ -3,15 +3,19 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class Event {
-  final String title;
-  final String description;
+  final String name;
+  final String email;
   final DateTime from;
   final DateTime to;
+  final String mobile;
+  final String comment;
   final Color backgroundColor;
 
   const Event({
-    required this.title,
-    required this.description,
+    required this.name,
+    required this.email,
+    required this.mobile,
+    required this.comment,
     required this.from,
     required this.to,
     this.backgroundColor = Colors.green,
